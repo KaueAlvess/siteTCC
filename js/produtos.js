bea403fa039bd5a72e2acbe04dbbd94b5ce3ab77
kaue <kaue.rocha22@etec.sp.gov.br>
2025-06-05 15:33:38 -0300

    estetica

--- js/produtos.js
+++
@@ -1,8 +0,0 @@
-// Crie um arquivo produtos.js e adicione ao HTML
-document.querySelectorAll('.produto-item button').forEach(btn => {
-    btn.addEventListener('click', function() {
-        const nomeProduto = this.closest('.produto-info').querySelector('h3').textContent;
-        alert(`${nomeProduto} adicionado ao carrinho!`);
-        // (Podemos redirecionar para o carrinho ou atualizar um contador)
-    });
-});
